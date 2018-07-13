f1a630d587188525ba93a78cf76cd662550ff1c5
AlexeyZharikov <mefis2.0@gmail.com>
2018-07-13 20:07:43 +0300

    4

--- server.js
+++ server.js
@@ -28,13 +28,5 @@ app.listen(3000);
 
 
 
-// let http = require('http');
 
-// let server = http.createServer((request, response) => {
-//     response.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'});
-//     let myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf-8');
-//     myReadShort.pipe(response);
-// });
-
-// server.listen(3000, '127.0.0.1');
 console.log("port 3000");
