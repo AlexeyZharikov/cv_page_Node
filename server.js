6168dba6f6c5c6242d8081e1d87f9fee31388358
AlexeyZharikov <mefis2.0@gmail.com>
2018-07-13 20:02:33 +0300

    3

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
+
+let express = require('express');
 let fs = require('fs');
+let app = express();
 
+app.use(express.static('static'));
 
-let http = require('http');
+app.get('/', (request,response)=>{
+    response.sendFile(__dirname + '/static/index.html', 'utf-8');
 
-let server = http.createServer((request, response) => {
-    response.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'});
-    let myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf-8');
-    myReadShort.pipe(response);
+});
+app.get('/cv', (request,response)=>{
+    response.sendFile(__dirname + '/static/index.html', 'utf-8');
+
+});
+app.get('/projects', (request,response)=>{
+    response.sendFile(__dirname + '/static/index.html', 'utf-8');
+
 });
 
-server.listen(3000, '127.0.0.1');
+app.get('/contact', (request,response)=>{
+    response.sendFile(__dirname + '/static/index.html', 'utf-8');
+
+});
+
+app.listen(3000);
+
+
+
+
+// let http = require('http');
+
+// let server = http.createServer((request, response) => {
+//     response.writeHead(200,{'Content-Type': 'text/html; charset=utf-8'});
+//     let myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf-8');
+//     myReadShort.pipe(response);
+// });
+
+// server.listen(3000, '127.0.0.1');
 console.log("port 3000");
