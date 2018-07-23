6b1937003760010dd3b9d23001210e7ebd5c7639
AlexeyZharikov <mefis2.0@gmail.com>
2018-07-24 01:25:39 +0300

    8

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 let bodyParser = require('body-parser');
 let express = require('express');
+let nodemailer = require('nodemailer');
+
 let app = express();
 
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
@@ -7,24 +9,57 @@ let urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use('/static', express.static('static'));
 app.set('view engine', 'ejs');
 
-app.get('/', (request,response)=>{
+app.get('/', (request, response) => {
     response.render('index');
 
 });
-app.get('/cv', (request,response)=>{
+app.get('/cv', (request, response) => {
     response.render('cv');
 
 });
-app.get('/projects', (request,response)=>{
+app.get('/projects', (request, response) => {
     response.render('project');
 
 });
 
-app.get('/contact', (request,response)=>{
+app.get('/contact', (request, response) => {
     response.render('contact');
 
 });
-app.post("/contact", urlencodedParser, (request, response)=>{
+app.post('/contact', urlencodedParser, (request, response) => {
+
+    let sentMes = `
+        <p>name: ${request.body.name}</p>
+        <p>E-mail: ${request.body.email}</p>
+        <p>messgae: ${request.body.message}</p>
+        `;
+
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        auth: {
+            user: '*****@gmail.com',
+            pass: '****'
+        }
+    });
+
+    let mailOptions = {
+        from: 'my CV-page<mefis2.0@gmail.com>',
+        to: 'mefis2.0@gmail.com',
+        subject: 'You have new message from CV-page',
+        text: 'you have new message!',
+
+        html: sentMes
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+
+    });
+
+
+
     console.log(request.body);
     response.render('contact');
 });
@@ -32,7 +67,3 @@ app.post("/contact", urlencodedParser, (request, response)=>{
 app.listen(3000);
 
 
-
-
-
-console.log("port 3000");
